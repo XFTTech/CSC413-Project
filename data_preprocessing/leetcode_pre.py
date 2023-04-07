5c9407ea436dc1954a2df291fa5dfc22f0336a81
zhuyuezx <zhuyuezx@gmail.com>
2023-04-07 18:23:30 -0400

    working on data preprocessing

--- data_preprocessing/leetcode_pre.py
+++ data_preprocessing/leetcode_pre.py
@@ -7,31 +7,41 @@ def read_data(file: str):
     df = pd.read_csv(raw_data_path + file)
     return df
 
-if __name__ == '__main__':
-    leetcode_data = read_data('leetcode_questions.csv')
-    columns = leetcode_data.columns
-    # needed columns: ['Question ID', 'Question Title', 'Question Slug', 'Topic Tagged text', 'Question Text', 'Similar Questions ID']
+def process_data(leetcode_data: pd.DataFrame):
+    print(leetcode_data.columns)
+    # needed columns: ['id', 'title', 'description', 'related_topics']
     # drop unnecessary columns
-    leetcode_data = leetcode_data[['Question Title', 'Question Slug', 'Topic Tagged text', 'Question Text']]
-    # first drop rows with no Question Text or Topic Tagged text
+    leetcode_data = leetcode_data[['id', 'title', 'description', 'related_topics']]
+    # first drop rows with no description or related topics
     for index, row in leetcode_data.iterrows():
-        if pd.isnull(row['Question Text']) or pd.isnull(row['Topic Tagged text']):
+        if pd.isnull(row['description']) or pd.isnull(row['related_topics']):
             leetcode_data = leetcode_data.drop(index)
     
-    all_topics = set()
+    all_topics = {}
     for index, row in leetcode_data.iterrows():
-        topics = str(row['Topic Tagged text']).split(',')
+        topics = str(row['related_topics']).split(',')
         for topic in topics:
-            all_topics.add(topic)
+            if topic not in all_topics:
+                all_topics[topic] = 1
+            else:
+                all_topics[topic] += 1
+    # sort topics by frequency
+    all_topics = sorted(all_topics.items(), key=lambda x: x[1], reverse=True)
+    all_topics_keys = [topic[0] for topic in all_topics]
     # represent topics using 1 and 0 using each topic as a column
-    for topic in all_topics:
+    for topic in all_topics_keys:
         leetcode_data[topic] = 0
     for index, row in leetcode_data.iterrows():
-        topics = str(row['Topic Tagged text']).split(',')
+        topics = str(row['related_topics']).split(',')
         for topic in topics:
             leetcode_data.at[index, topic] = 1
-    # drop Topic Tagged text column
-    leetcode_data = leetcode_data.drop(columns=['Topic Tagged text'])
+    # drop related_topics column
+    leetcode_data = leetcode_data.drop(columns=['related_topics'])
     print(leetcode_data.head())
+    return leetcode_data
+
+if __name__ == '__main__':
+    leetcode_data = read_data('leetcode_dataset - lc.csv')
+    leetcode_data = process_data(leetcode_data)
     # save to csv
     leetcode_data.to_csv('data/leetcode.csv', index=False)
